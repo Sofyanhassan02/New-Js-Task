7c7881c933a929afd007671a5d121b610b32501e
Sofyanhassan02 <sofyanhassan60@gmail.com>
2024-03-11 15:22:41 +0200

    first commit

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+// let arr = [{ name: "laptop1", description: "pc" }, { name: "phone1", description: "cellphone" }, { name: "headphones1", description: "accessories" }];
+// let datavalue = prompt("enter name of product");
+// let details = arr.filter(function(ele, index, arr) {
+//   if (ele.name.includes(datavalue)) {
+//     return ele;
+//   } else {
+//     console.log("Not Found");
+//   }
+// });
+// console.log(details);
+
